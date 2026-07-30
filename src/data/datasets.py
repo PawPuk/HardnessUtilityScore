Fixed a bug where normalisation would be applied multiple times. Simplified IndexedDataset. Now it's slower in some cases but less prone to bugs.

src/data/datasets.py
@@ -12,7 +12,7 @@ from torch.utils.data import TensorDataset
 class IndexedDataset(torch.utils.data.Dataset):
     def __init__(self, dataset, is_test=False, transform=None):
         # To improve speed, we transform the dataset into a TensorDataset (only viable if no augmentation is applied)
-        if not isinstance(dataset, TensorDataset) and is_test:
+        """if not isinstance(dataset, TensorDataset) and is_test:
             data_list, label_list = [], []
             for i in range(len(dataset)):
                 data, label = dataset[i]
@@ -20,7 +20,7 @@ class IndexedDataset(torch.utils.data.Dataset):
                 label_list.append(torch.tensor(label))  # Necessary because some datasets return labels as integers
             data_tensor = torch.stack(data_list)
             label_tensor = torch.tensor(label_list)
-            dataset = TensorDataset(data_tensor, label_tensor)
+            dataset = TensorDataset(data_tensor, label_tensor)"""
 
         self.dataset = dataset
         self.transform = transform

src/experiments/downstream_task.py
@@ -22,8 +22,8 @@ def main(dataset_name: str, generative_model: str, oversampling_strategy: str, a
     num_training_samples = config['num_training_samples']
     dataset_count = config['num_datasets']
 
-    _, training_set, test_loader, test_set = load_real_dataset(dataset_name, True, True)
-    _, synthetic_set = load_synthetic_dataset(dataset_name, generative_model)
+    _, training_set, test_loader, _ = load_real_dataset(dataset_name)
+    _, synthetic_set = load_synthetic_dataset(dataset_name, generative_model, False)
 
     labels = [training_set[idx][1] for idx in range(len(training_set))]
     # Not doing np.mean() here to match the in_hoc_estimates obtained from compute_resampling_ratios.py
@@ -46,8 +46,6 @@ def main(dataset_name: str, generative_model: str, oversampling_strategy: str, a
         augmented_resampled_dataset = perform_data_augmentation(resampled_dataset, dataset_name)
         resampled_loaders.append(get_dataloader(augmented_resampled_dataset, batch_size=config['batch_size'],
                                                 shuffle=True))
-
-    test_loader = get_dataloader(test_set, batch_size=config['batch_size'])
 
     save_suffix = f'{oversampling_strategy}_{generative_model}_{alpha:.2f}'
     trainer = ModelTrainer(len(training_set), resampled_loaders, test_loader, dataset_name, save_suffix,

src/measures/frechet_inception_distance.py
@@ -42,7 +42,7 @@ def load_feature_extractor(
 def extract_features(
         loader: torch.utils.data.DataLoader,
         model: Union[Inception3, ResNet18LowRes],
-        normalize: bool,
+        weird_normalization: bool,
         dataset_mean: Tuple[float, float, float],
         dataset_std: Tuple[float, float, float],
         desc: str = "Extracting features"
@@ -53,11 +53,13 @@ def extract_features(
         for images, _, _ in tqdm(loader, desc=desc):
             if isinstance(model, Inception3):
                 images = TF.resize(images, (299, 299))
-            if normalize:
-                images_raw = (images * dataset_std) + dataset_mean
+            if weird_normalization:
                 mean = torch.tensor([0.485, 0.456, 0.406], device=DEVICE).view(1, 3, 1, 1)
                 std = torch.tensor([0.229, 0.224, 0.225], device=DEVICE).view(1, 3, 1, 1)
-                images = (images_raw - mean) / std
+            else:
+                mean = torch.tensor(dataset_mean, device=DEVICE).view(1, 3, 1, 1)
+                std = torch.tensor(dataset_std, device=DEVICE).view(1, 3, 1, 1)
+            images = (images - mean) / std
             images = images.to(DEVICE)
 
             feat = model(images)
@@ -70,7 +72,7 @@ def compute_fid(
         real_loader: torch.utils.data.DataLoader,
         gen_loader: torch.utils.data.DataLoader,
         model_type: str = 'InceptionV3',
-        normalize: bool = False
+        weird_normalization: bool = False
 ) -> float:
     config = get_config(dataset_name)
     num_classes = config['num_classes']
@@ -81,8 +83,8 @@ def compute_fid(
     model = load_feature_extractor(dataset_name, num_classes, model_type)
 
     # Extract features
-    feat_real = extract_features(real_loader, model, normalize, mean, std, "Real (FID)")
-    feat_gen = extract_features(gen_loader, model, normalize, mean, std, "Generated (FID)")
+    feat_real = extract_features(real_loader, model, weird_normalization, mean, std, "Real (FID)")
+    feat_gen = extract_features(gen_loader, model, weird_normalization, mean, std, "Generated (FID)")
 
     mu1, sigma1 = np.mean(feat_real, axis=0), np.cov(feat_real, rowvar=False)
     mu2, sigma2 = np.mean(feat_gen, axis=0), np.cov(feat_gen, rowvar=False)

src/utils/evaluation.py
@@ -65,13 +65,12 @@ def compute_sample_allocation_for_resampling(
     samples_per_class = [int(round(ratio * num_training_samples)) for ratio in ratios.values()]
 
     # Tailor the degree of the introduces data imbalance (only applicable if alpha is larger than 1).
-    if alpha > 1.0:
-        average_sample_count = int(np.mean(samples_per_class))
-        for class_id in range(num_classes):
-            absolute_difference = abs(samples_per_class[class_id] - average_sample_count)
-            if samples_per_class[class_id] > average_sample_count:
-                samples_per_class[class_id] = average_sample_count + int(alpha * absolute_difference)
-            else:
-                samples_per_class[class_id] = average_sample_count - int(alpha * absolute_difference)
+    average_sample_count = int(np.mean(samples_per_class))
+    for class_id in range(num_classes):
+        absolute_difference = abs(samples_per_class[class_id] - average_sample_count)
+        if samples_per_class[class_id] > average_sample_count:
+            samples_per_class[class_id] = average_sample_count + int(alpha * absolute_difference)
+        else:
+            samples_per_class[class_id] = average_sample_count - int(alpha * absolute_difference)
 
     return samples_per_class